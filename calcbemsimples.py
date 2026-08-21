003f4542563f186d517a9d2b44e3b794834cb48f
David Dias Barbosa Filho <1352619285@ulife.com.br>
2026-08-21 08:57:52 -0300

    commit

---
+++ calcbemsimples.py
@@ -0,0 +1,18 @@
+# Função da calculadora simples
+n1 = float(input("Digite o primeiro número: "))
+op = input("Escolha a operação (+, -, *, /): ")
+n2 = float(input("Digite o segundo número: "))
+
+if op == "+":
+    print("Resultado:", n1 + n2)
+elif op == "-":
+    print("Resultado:", n1 - n2)
+elif op == "*":
+    print("Resultado:", n1 * n2)
+elif op == "/":
+    if n2 != 0:
+        print("Resultado:", n1 / n2)
+    else:
+        print("Erro: Divisão por zero!")
+else:
+    print("Operação inválida!")
